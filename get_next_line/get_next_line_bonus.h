369de7ac7b647e1becb49b7d478fe286ec2496d8
Mehmet Yücem Aydın <73720000+t4kinr4@users.noreply.github.com>
2022-12-06 14:24:26 +0300

    son

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mehaydin <mehaydin@student.42kocaeli.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/06 14:19:47 by mehaydin          #+#    #+#             */
+/*   Updated: 2022/12/06 14:19:47 by mehaydin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+char	*get_next_line(int fd);
+
+#endif
